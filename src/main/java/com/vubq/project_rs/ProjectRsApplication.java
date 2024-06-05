bae1593d35227098d0cdc5545ab7e0370cd8ba89
vubq <vubq242002@gmail.com>
2024-06-05 14:05:46 +0700

    Init project BE

---
+++ src/main/java/com/vubq/project_rs/ProjectRsApplication.java
@@ -0,0 +1,13 @@
+package com.vubq.project_rs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectRsApplication.class, args);
+    }
+
+}
